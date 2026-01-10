4495b78baf3a671f35cf6ad03a24441df8373fdc
Bao Pham <louis.phammm@gmail.com>
2026-01-10 15:02:12 +0700

    Base Deployment Config

---
+++ src/main/java/com/orion/helm/chart/demo/OrionHelmChartDemoApplication.java
@@ -0,0 +1,13 @@
+package com.orion.helm.chart.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrionHelmChartDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrionHelmChartDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orion/helm/chart/demo/OrionHelmChartDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orion.helm.chart.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrionHelmChartDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
